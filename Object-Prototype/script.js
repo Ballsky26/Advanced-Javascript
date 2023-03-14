17cab2869b5c2abbb95c1d3cc137623ba806ec50
Ballsky22 <ballskyteam22@gmail.com>
2023-03-14 10:24:26 +0700

    Advanced Javascript Object Create

--- Object-Prototype/script.js
+++ Object-Prototype/script.js
@@ -1,5 +1,6 @@
 // Cara muntuk membuat Object pada Javascript
 // 1. Object Literal
+// PROBLEM : Tidak Efektif untuk object Banyak
 // let orang1 = {
 //   name: "Ballsky",
 //   energy: 23,
@@ -18,39 +19,46 @@
 // };
 
 // 2. Function Declaration
-// function Player(nama, energi) {
-//   let player = {};
-//   player.nama = nama;
-//   player.energi = energi;
+const methodPlayer = {
+  makan: function (porsi) {
+    this.energi += porsi;
+    console.log(`Hallo ${this.nama}, Selamat Makan`);
+  },
+  main: function (jam) {
+    this.energi -= jam;
+    console.log(`Hallo ${this.nama}, Selamat Bermain`);
+  },
 
-//   player.makan = function (porsi) {
-//     this.energi += porsi;
-//     console.log(`Hallo ${this.nama}, Selamat Makan`);
-//   };
-//   player.main = function (jam) {
-//     this.energi -= jam;
-//     console.log(`Hallo ${this.nama}, Selamat Bermain`);
-//   };
-//   return player;
-// }
+  tidur: function (jam) {
+    this.energi += jam * 2;
+    console.log(`Hallo ${this.nama}, Selamat Tidur`);
+  },
+};
+function Player(nama, energi) {
+  let player = Object.create(methodPlayer);
+  player.nama = nama;
+  player.energi = energi;
 
-// let ballsky = Player("Ballsky", 10);
-// let Fahdil = Player("Fahdil", 20);
+  return player;
+}
+
+let ballsky = Player("Ballsky", 10);
+let Fahdil = Player("Fahdil", 20);
 
 // 3. Constructor Function
 // Keyword New
-function Player(nama, energi) {
-  this.nama = nama;
-  this.energi = energi;
+// function Player(nama, energi) {
+//   this.nama = nama;
+//   this.energi = energi;
 
-  this.makan = function (porsi) {
-    this.energi += porsi;
-    console.log(`Hallo ${this.nama}, Selamat Makan`);
-  };
-  this.main = function (jam) {
-    this.energi -= jam;
-    console.log(`Hallo ${this.nama}, Selamat Bermain`);
-  };
-}
+//   this.makan = function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Hallo ${this.nama}, Selamat Makan`);
+//   };
+//   this.main = function (jam) {
+//     this.energi -= jam;
+//     console.log(`Hallo ${this.nama}, Selamat Bermain`);
+//   };
+// }
 
-let ballsky = new Player("Ballsky", 10);
+// let ballsky = new Player("Ballsky", 10);
