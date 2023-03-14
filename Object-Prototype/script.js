24f123dd6436f6f7636a32b8c36061d1783c0656
Ballsky22 <ballskyteam22@gmail.com>
2023-03-14 21:53:24 +0700

    Advanced Javascript Prototype

--- Object-Prototype/script.js
+++ Object-Prototype/script.js
@@ -19,31 +19,80 @@
 // };
 
 // 2. Function Declaration
-const methodPlayer = {
-  makan: function (porsi) {
-    this.energi += porsi;
-    console.log(`Hallo ${this.nama}, Selamat Makan`);
-  },
-  main: function (jam) {
-    this.energi -= jam;
-    console.log(`Hallo ${this.nama}, Selamat Bermain`);
-  },
+// const methodPlayer = {
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Hallo ${this.nama}, Selamat Makan`);
+//   },
+//   main: function (jam) {
+//     this.energi -= jam;
+//     console.log(`Hallo ${this.nama}, Selamat Bermain`);
+//   },
 
-  tidur: function (jam) {
-    this.energi += jam * 2;
-    console.log(`Hallo ${this.nama}, Selamat Tidur`);
-  },
-};
+//   tidur: function (jam) {
+//     this.energi += jam * 2;
+//     console.log(`Hallo ${this.nama}, Selamat Tidur`);
+//   },
+// };
+// function Player(nama, energi) {
+//   let player = Object.create(methodPlayer);
+//   player.nama = nama;
+//   player.energi = energi;
+
+//   return player;
+// }
+
+// let ballsky = Player("Ballsky", 10);
+// let Fahdil = Player("Fahdil", 20);
+
+// Versi Prototype
 function Player(nama, energi) {
-  let player = Object.create(methodPlayer);
-  player.nama = nama;
-  player.energi = energi;
-
-  return player;
+  this.nama = nama;
+  this.energi = energi;
 }
+Player.prototype.makan = function (porsi) {
+  this.energi += porsi;
+  return `Halo ${this.nama}, Selamat Makan`;
+};
 
-let ballsky = Player("Ballsky", 10);
-let Fahdil = Player("Fahdil", 20);
+Player.prototype.main = function (jam) {
+  this.energi -= jam;
+  return `Halo ${this.nama}, Selamat Bermain`;
+};
+
+Player.prototype.tidur = function (jam) {
+  this.energi += jam;
+  return `Halo ${this.nama}, Selamat Tidur`;
+};
+let ballsky = new Player("Ballsky", 10);
+
+// Versi Class
+// class Player {
+//   constructor(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+//   }
+//   makan(porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, Selamat Makan`;
+//   }
+
+//   main(jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, Selamat Bermain`;
+//   }
+
+//   tidur(jam) {
+//     this.energi += jam;
+//     return `Halo ${this.nama}, Selamat Tidur`;
+//   }
+// }
+
+// let ballsky = new Player("Ballsky", 10);
+// let Fahdil = new Player("Fahdil", 20);
+
+// let angka = [1, 2, 3];
+// console.log(angka.sort());
 
 // 3. Constructor Function
 // Keyword New
