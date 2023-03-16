a2da2c917a26f1dafd665f71c533551595c4d592
Ballsky22 <ballskyteam22@gmail.com>
2023-03-16 22:26:58 +0700

    Advanced Javascript Latihan Filter Map Reduce

--- Filter-Map-Reduce/script.js
+++ Filter-Map-Reduce/script.js
@@ -34,9 +34,9 @@
 // Cari angka > 5
 // Hasil kalikan 3
 // Jumlahkan
-const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
-const hasil = angka
-  .filter((a) => a > 5) //8, 9, 9
-  .map((a) => a * 3) //24, 27, 27
-  .reduce((acc, cur) => acc + cur); // 78
-console.log(hasil);
+// const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
+// const hasil = angka
+//   .filter((a) => a > 5) //8, 9, 9
+//   .map((a) => a * 3) //24, 27, 27
+//   .reduce((acc, cur) => acc + cur); // 78
+// console.log(hasil);
