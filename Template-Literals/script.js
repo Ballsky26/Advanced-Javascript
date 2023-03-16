c52eafe235dfb98a0b372e1b0cdef3a73aa388f6
Ballsky22 <ballskyteam22@gmail.com>
2023-03-16 23:38:30 +0700

    Advanced Javascript Tagged Templates

--- Template-Literals/script.js
+++ Template-Literals/script.js
@@ -1,6 +1,6 @@
 // Template Literals /  Template String
-const nama = "Ballsky";
-const umur = 23;
+// const nama = "Ballsky";
+// const umur = 23;
 // console.log(`Hallo, nama saya adalah ${nama}, dan saya umur ${umur} tahun.`);
 // console.log(
 //   "Hallo, nama saya adalah " + nama + ", dan saya berumur " + umur + " tahun."
@@ -14,14 +14,50 @@ const umur = 23;
 // console.log(`${x % 2 == 0 ? "genap" : "ganjil"}`);
 
 // HTML Fragments
-const mhs = {
-  nama: "Ballsky",
-  umur: 23,
-  nim: "18.240.0015",
-};
+// const mhs = {
+//   nama: "Ballsky",
+//   umur: 23,
+//   nim: "18.240.0015",
+// };
 
-const el = `<div class="mhs">
-  <h2>${mhs.nama}</h2>
-  <span class="nrp">${mhs.nim}</span>
-  </div>`;
-console.log(el);
+// const el = `<div class="mhs">
+//   <h2>${mhs.nama}</h2>
+//   <span class="nrp">${mhs.nim}</span>
+//   </div>`;
+// console.log(el);
+
+// Tagged Templates
+// const nama = "Ballsky";
+// const umur = 23;
+
+// function coba(strings, ...values) {
+//   // let result = "";
+//   // strings.forEach((str, i) => {
+//   //   result += `${str}${values[i] || ""}`;
+//   // });
+//   // return result;
+
+//   return strings.reduce(
+//     (result, str, i) => `${result}${str}${values[i] || ""}`,
+//     ""
+//   );
+// }
+
+// const str = coba`Halo, nama saya ${nama}, saya berumur ${umur} tahun.`;
+// console.log(str);
+
+// Highlight
+const nama = "Ballsky";
+const umur = 23;
+const email = "Ballsky@gmail.com";
+
+function highlight(strings, ...values) {
+  return strings.reduce(
+    (result, str, i) =>
+      `${result}${str}<span class="hl">${values[i] || ""}</span>`,
+    ""
+  );
+}
+
+const str = highlight`Halo, nama saya ${nama}, saya berumur ${umur} tahun, dan email saya adalah : ${email}`;
+document.body.innerHTML = str;
