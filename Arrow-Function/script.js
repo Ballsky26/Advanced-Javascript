eb33efbd42c461d67551f1a524ececfa639338ef
Ballsky22 <ballskyteam22@gmail.com>
2023-03-16 21:05:05 +0700

    Advanced Javascript this pada Arrow Function

--- Arrow-Function/script.js
+++ Arrow-Function/script.js
@@ -24,7 +24,7 @@
 // const tampilNama = () => "Hello World";
 // console.log(tampilNama());
 
-let player = ["Ballsky", "Ari", "Miki"];
+// let player = ["Ballsky", "Ari", "Miki"];
 
 // let jumlahHuruf = player.map(function (nama) {
 //   return nama.length;
@@ -34,5 +34,73 @@ let player = ["Ballsky", "Ari", "Miki"];
 // let jumlahHuruf = player.map((nama) => nama.length);
 // console.log(jumlahHuruf);
 
-let jumlahHuruf = player.map((nama) => ({ nama: nama, jmlHuruf: nama.length }));
-console.table(jumlahHuruf);
+// let jumlahHuruf = player.map((nama) => ({ nama: nama, jmlHuruf: nama.length }));
+// console.table(jumlahHuruf);
+
+// Konsep This pada Arrow Function
+
+// Constructor Function
+// const Player = function () {
+//   this.nama = "Ballsky";
+//   this.nomor = 26;
+//   this.sayHello = function () {
+//     console.log(
+//       `Hallo, Nama saya ${this.nama}, dan nomor Balap saya ${this.nomor}`
+//     );
+//   };
+// };
+
+// const ballsky = new Player();
+
+// Arrow Function
+// const Player = function () {
+//   this.nama = "Ballsky";
+//   this.nomor = 26;
+//   this.sayHello = () => {
+//     console.log(
+//       `Hallo, Nama saya ${this.nama}, dan nomor Balap saya ${this.nomor}`
+//     );
+//   };
+// };
+
+// const ballsky = new Player();
+
+// Object Literal
+
+// const player1 = {
+//   nama: "Ballsky",
+//   nomor: 26,
+//   sayHello: () => {
+//     console.log(
+//       `Hallo, Nama saya ${this.nama}, dan nomor Balap saya ${this.nomor}`
+//     );
+//   },
+// };
+
+// const Player = function () {
+//   this.nama = "Ballsky";
+//   this.nomor = 26;
+//   this.sayHello = function () {
+//     console.log(
+//       `Hallo, Nama saya ${this.nama}, dan nomor Balap saya ${this.nomor}`
+//     );
+//   };
+//   setInterval(() => {
+//     console.log(this.nomor++);
+//   }, 500);
+// };
+// const ballsky = new Player();
+
+const box = document.querySelector(".box");
+box.addEventListener("click", function () {
+  let satu = "size";
+  let dua = "caption";
+
+  if (this.classList.contains(satu)) {
+    [satu, dua] = [dua, satu];
+  }
+  this.classList.toggle(satu);
+  setTimeout(() => {
+    this.classList.toggle(dua);
+  }, 600);
+});
