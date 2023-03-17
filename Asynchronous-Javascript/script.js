2f064cf2ae6041185af1df9592f7dd395fca1728
Ballsky22 <ballskyteam22@gmail.com>
2023-03-17 23:18:49 +0700

    Advanced Javascript Asynchronous Javascript

---
+++ Asynchronous-Javascript/script.js
@@ -0,0 +1,23 @@
+// console.log("satu");
+// console.log("dua");
+// console.log("tiga");
+
+// console.log("satu");
+// setTimeout(function tunggu() {
+//   console.log("dua");
+// }, 5000);
+// console.log("tiga");
+
+// $.on("button", "click", function onClick(){
+//     setTimeout function timer(){
+//         console.log("you clicked the button !");
+//     }, 2000);
+// });
+
+// console.log("Hi");
+
+// setTimeout(function timeout(){
+//     console.log("Click the button!");
+// }, 5000);
+
+// console.log("Welcome to World");
