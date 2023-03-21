b4c90e76bab67f2c18e5229b5a49a985318ac5e9
Ballsky22 <ballskyteam22@gmail.com>
2023-03-21 22:44:32 +0700

    Practice Fetch Membuat fungsi detail Film menggunakan Fetch

---
+++ Practice-Fetch-Refactor/script.js
@@ -0,0 +1,117 @@
+// Fetch
+
+// const searchButton = document.querySelector(".search-button");
+// searchButton.addEventListener("click", function () {
+//   const inputKeyword = document.querySelector(".input-keyword");
+//   fetch("http://www.omdbapi.com/?apikey=efc47ecd&s=" + inputKeyword.value)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       const movies = response.Search;
+//       let cards = "";
+//       movies.forEach((m) => (cards += showCards(m)));
+//       const movieContainer = document.querySelector(".movie-container");
+//       movieContainer.innerHTML = cards;
+
+// Ketika tombol detail di klik
+//       const modalDetailButton = document.querySelectorAll(
+//         ".modal-detail-button"
+//       );
+//       modalDetailButton.forEach((btn) => {
+//         btn.addEventListener("click", function () {
+//           const imdbid = this.dataset.imdbid;
+//           fetch("http://www.omdbapi.com/?apikey=efc47ecd&i=" + imdbid)
+//             .then((response) => response.json())
+//             .then((m) => {
+//               const movieDetail = showMovieDetail(m);
+//               const modalBody = document.querySelector(".modal-body");
+//               modalBody.innerHTML = movieDetail;
+//             });
+//         });
+//       });
+//     });
+// });
+
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async function () {
+  const inputKeyword = document.querySelector(".input-keyword");
+  const movies = await getMovies(inputKeyword.value);
+  updateUI(movies);
+});
+
+// event binding
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    updateUIDetail(movieDetail);
+  }
+});
+
+function getMovieDetail(imdbid) {
+  return fetch("http://www.omdbapi.com/?apikey=efc47ecd&i=" + imdbid)
+    .then((response) => response.json())
+    .then((m) => m);
+}
+
+function updateUIDetail(m) {
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = movieDetail;
+}
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=efc47ecd&s=" + keyword)
+    .then((response) => response.json())
+    .then((response) => response.search);
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+    <div class="card">
+      <img class="card-img-top" src="${m.Poster}" alt="" />
+      <div class="card-body">
+        <h5 class="card-title">${m.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal"
+        data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Lihat Selengkapnya</a>
+      </div>
+    </div>
+  </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="${m.Poster}" class="img-fluid" alt="" />
+      </div>
+      <div class="col-md">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <h4>${m.Title} (${m.Year})</h4>
+          </li>
+          <li class="list-group-item">
+            <strong>Director : </strong> ${m.Director}
+          </li>
+          <li class="list-group-item">
+            <strong>Actors : </strong> ${m.Actors}
+          </li>
+          <li class="list-group-item">
+            <strong>Writer : </strong>${m.Writer}
+          </li>
+          <li class="list-group-item">
+            <strong>Plot : </strong><br>
+            ${m.Plot}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>`;
+}
