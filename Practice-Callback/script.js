ceedcc332280df7f7f595aa1227bffe49c1e7498
Ballsky22 <ballskyteam22@gmail.com>
2023-03-21 21:25:43 +0700

    Membuat detail film

--- Practice-Callback/script.js
+++ Practice-Callback/script.js
@@ -4,21 +4,70 @@ $.ajax({
     const movies = results.Search;
     let cards = "";
     movies.forEach((m) => {
-      cards += `<div class="col-md-4 my-3">
-        <div class="card">
-          <img class="card-img-top" src="${m.Poster}" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">${m.Title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-            <a href="#" class="btn btn-primary" data-toggle="modal"
-            data-target="#movieDetailModal">Lihat Selengkapnya</a>
-          </div>
-        </div>
-      </div>`;
+      cards += showCards(m);
     });
     $(".movie-container").html(cards);
+
+    // Ketika tombol detail di klik
+    $(".modal-detail-button").on("click", function () {
+      $.ajax({
+        url:
+          "http://www.omdbapi.com/?apikey=efc47ecd&i=" + $(this).data("imdbid"),
+        success: (m) => {
+          const movieDetail = showMovieDetail(m);
+          $(".modal-body").html(movieDetail);
+        },
+        error: (e) => {
+          console.log(e.responseText);
+        },
+      });
+    });
   },
   error: (e) => {
     console.log(e.responseText);
   },
 });
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+    <div class="card">
+      <img class="card-img-top" src="${m.Poster}" alt="" />
+      <div class="card-body">
+        <h5 class="card-title">${m.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+        <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal"
+        data-target="#movieDetailModal" data-imdbid="${m.imdbID}">Lihat Selengkapnya</a>
+      </div>
+    </div>
+  </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="${m.Poster}" class="img-fluid" alt="" />
+      </div>
+      <div class="col-md">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <h4>${m.Title} (${m.Year})</h4>
+          </li>
+          <li class="list-group-item">
+            <strong>Director : </strong> ${m.Director}
+          </li>
+          <li class="list-group-item">
+            <strong>Actors : </strong> ${m.Actors}
+          </li>
+          <li class="list-group-item">
+            <strong>Writer : </strong>${m.Writer}
+          </li>
+          <li class="list-group-item">
+            <strong>Plot : </strong><br>
+            ${m.Plot}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>`;
+}
