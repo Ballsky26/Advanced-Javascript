35224ddbaea48c2c4a334ba165aca89fdca3f707
Ballsky22 <ballskyteam22@gmail.com>
2023-03-21 23:29:21 +0700

    Practice Fetch dengan Error Handling

--- Practice-Fetch-Refactor/script.js
+++ Practice-Fetch-Refactor/script.js
@@ -1,10 +1,37 @@
 // Fetch
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async function () {
-  const inputKeyword = document.querySelector(".input-keyword");
-  const movies = await getMovies(inputKeyword.value);
-  updateUI(movies);
+  try {
+    const inputKeyword = document.querySelector(".input-keyword");
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
+  } catch (err) {
+    alert(err);
+  }
 });
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=efc47ecd&s=" + keyword)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((response) => {
+      if (response.Response === "False") {
+        throw new Error(response.Error);
+      }
+      return response.Search();
+    });
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
 
 // event binding
 document.addEventListener("click", async function (e) {
@@ -25,19 +52,6 @@ function updateUIDetail(m) {
   const movieDetail = showMovieDetail(m);
   const modalBody = document.querySelector(".modal-body");
   modalBody.innerHTML = movieDetail;
-}
-
-function getMovies(keyword) {
-  return fetch("http://www.omdbapi.com/?apikey=efc47ecd&s=" + keyword)
-    .then((response) => response.json())
-    .then((response) => response.search);
-}
-
-function updateUI(movies) {
-  let cards = "";
-  movies.forEach((m) => (cards += showCards(m)));
-  const movieContainer = document.querySelector(".movie-container");
-  movieContainer.innerHTML = cards;
 }
 
 function showCards(m) {
